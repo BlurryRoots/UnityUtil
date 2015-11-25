7f25f8a2643ca1c6e317c68a3e988a46f6e91b3f
blurryroots <blurryroots@posteo.de>
2015-11-25 13:15:58 +0100

    extracted procedural code from main project to sub project
    also removed project dungeon code and test

---
+++ Runtime.Procedural/BlurryRoots/Procedural/Chain.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace BlurryRoots.Procedural {
+
+	// TODO: split data and processing ?!
+	[System.Serializable]
+	public class Chain<TValueType> : List<IChainLink<TValueType>>, IChainLink<TValueType> {
+
+		public TValueType Process (TValueType input) {
+			var result = input;
+
+			foreach (var node in this) {
+				result = node.Process (result);
+			}
+
+			return result;
+		}
+
+		public Chain<TValueType> Link (IChainLink<TValueType> element) {
+			this.Add (element);
+
+			return this;
+		}
+
+	}
+
+}
---
+++ Runtime.Procedural/BlurryRoots/Procedural/IChainLink.cs
@@ -0,0 +1,18 @@
+﻿namespace BlurryRoots.Procedural {
+
+	/// <summary>
+	/// Basis for any chain element meant to be linked together.
+	/// </summary>
+	/// <typeparam name="TValueType">Type of value to process.</typeparam>
+	public interface IChainLink<TValueType> {
+
+		/// <summary>
+		/// Processes the given value and outputs the result.
+		/// </summary>
+		/// <param name="input">Value to process.</param>
+		/// <returns>Processes value.</returns>
+		TValueType Process (TValueType input);
+
+	}
+
+}
