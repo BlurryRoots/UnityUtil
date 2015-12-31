315a2a4e0ca7c4ff044fabd02a296524d2e86b1a
blurryroots <blurryroots@posteo.de>
2015-12-31 10:27:29 +0100

    added chain interface, removed unity engine library dependency

---
+++ Runtime.Procedural/BlurryRoots/Procedural/IChain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlurryRoots.Procedural {
+
+    public interface IChain<TValueType, TLinkType> {
+
+        /// <summary>
+		/// Links (apends) a new chain link element.
+		/// </summary>
+		/// <param name="element">Element to link to chain.</param>
+		/// <returns>Reference to chain itself.</returns>
+		IChain<TValueType, TLinkType> Link (TLinkType element);
+
+        /// <summary>
+        /// Get a reference to all elements linked in this chain.
+        /// </summary>
+        List<TLinkType> Elements {
+            get;
+        }
+
+    }
+
+}
